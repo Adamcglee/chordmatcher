1f0b70fa355954a8c928f8121abd8b213c09a33a
adamcglee <adamcglee@gmail.com>
2019-07-21 21:17:20 -0700

    added song search and set up api calls to return object of results and set state for searchresults

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Search from './components/search';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <h1>ChordMatcher</h1>
+      <Search/>
     </div>
   );
 }
---
+++ src/components/search.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchtext: "",
+      searchresults: []
+    };
+  }
+
+  inputHandler = async event => {
+    await this.setState({ [event.target.name]: event.target.value });
+    console.log("Search Text: ", this.state.searchtext);
+  };
+
+  searchSubmit = event => {
+    event.preventDefault();
+    const api_key = { headers: {"Guitarparty-Api-Key": process.env.REACT_APP_GUITARPARTY_API_KEY}};
+    axios
+      .get(
+        `http://api.guitarparty.com/v2/songs/?query=${this.state.searchtext}`,
+        api_key
+      )
+      .then(res => {
+        this.setState({ searchresults: res.data.objects });
+        console.log(this.state.searchresults);
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.searchSubmit}>
+          <h5>Search:</h5>
+          <input
+            type="text"
+            name="searchtext"
+            value={this.state.searchtext}
+            onChange={this.inputHandler}
+            placeholder="Search by Song"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
